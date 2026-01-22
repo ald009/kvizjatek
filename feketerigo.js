ee13dc4d432e0560a86d3af52ac2761bdec17ddc
Kovács Tibor <kovacs.tibor@diak.szbi-pg.hu>
2026-01-22 11:58:02 +0100

    most leht csinalni az ellenorzest

--- feketerigo.js
+++ feketerigo.js
@@ -1,7 +1,19 @@
 
 
 function agy(){
-    
-    kijelzo = document.getElementById("agy");
-    kijelzo.innerHTML = document.getElementById("range").value;
+    document.getElementById("agy").innerHTML = document.getElementById("matekrange").value;
+}
+
+
+var helyes = 0;
+function sub(){
+    if (document.getElementById("anyad").value.trim().toLowerCase() === "anyad")
+    {
+        document.getElementById("1").innerHTML = "Helyes!";
+        helyes += 1;
+    }
+    else
+    {
+        document.getElementById("1").innerHTML = "Rossz válasz!"
+    }
 }
