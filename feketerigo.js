1ef67e6b9ff4e0a9ebe6c31c7a5fd75481c6f9c5
ald009 <kovacs.tibor@diak.szbi-pg.hu>
2026-01-28 21:45:39 +0100

    vege van kicsi

--- feketerigo.js
+++ feketerigo.js
@@ -1,12 +1,14 @@
 
 
 function agy(){
-    document.getElementById("agy").innerHTML = document.getElementById("matekrange").value;
+    if (document.getElementById("matekrange").value === "250"){
+        document.getElementById("agy").innerHTML = "Minden órán";
+    }
+    else{
+        document.getElementById("agy").innerHTML = document.getElementById("matekrange").value;
+    }
 }
 
-function tetsz(){
-    document.getElementById("tetszik").innerHTML = document.getElementById("tetsz").value;
-}
 function jo(hany){
         document.getElementById(hany).innerHTML = "Helyes!";
         document.getElementById(hany).style.color = "green";
@@ -16,17 +18,27 @@ function jo(hany){
 function rossz(hany){
         document.getElementById(hany).innerHTML = "Rossz válasz!";
         document.getElementById(hany).style.color = "red";
-        
+    
 }
-
-
+function wat(hany){
+        document.getElementById(hany).innerHTML = "Azért ennyire nem gondoltam komolyan...";
+        document.getElementById(hany).style.color = "orange";
+        helyes += 1;
+}
 var helyes = 0;
 function sub(){
 
 
-    if (document.getElementById("anyad").value.trim().toLowerCase() === "anyad")
+    if (document.getElementById("anyad").value.trim().toLowerCase() !== "")
     {
-        jo("1");
+        if (document.getElementById("anyad").value.toLowerCase() === "horváth hajnalka" || document.getElementById("anyad").value.toLowerCase() === "havasné horváth hajnalka")
+        {
+            wat("1");
+        }
+        else
+        {
+            jo("1");
+        }
     }
     else
     {
@@ -52,6 +64,15 @@ function sub(){
         rossz("3");
     }
 
+    if (document.getElementById("bulcsuszam").value === "6")
+    {
+        jo("4");
+    }
+    else
+    {
+        rossz("4");
+    }
+
     if (document.getElementById("utal").checked === true)
     {
         jo("5");
@@ -70,7 +91,7 @@ function sub(){
         rossz("6");
     }
 
-    if (document.getElementById("tetszik").value > 0)
+    if (document.getElementById("paskolom").checked === true || document.getElementById("ketszeris").checked === true)
     {
         jo("7");
     }
@@ -78,6 +99,37 @@ function sub(){
     {
         rossz("7");
     }
+
+    if (document.getElementById("kopaszja").checked === true)
+    {
+        jo("8");
+    }
+    else
+    {
+        rossz("8");
+    }
+
+    if (document.getElementById("tokanylabel").innerHTML < "5")
+    {
+        jo("9");
+    }
+    else
+    {
+        rossz("9");
+    }
+
+    if (document.getElementById("szin").value === "#ff6a00")
+    {
+        jo("10");
+    }
+    else
+    {
+        rossz("10");
+    }
+
+alert("A helyes válaszok száma: " + helyes + "/10");
+
+helyes = 0;
         
 
 
